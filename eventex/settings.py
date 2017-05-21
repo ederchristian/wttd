933d91eff785117fbe148739f0fe683f1663e267
Eder Christian <ederchristian92@gmail.com>
2017-05-21 20:38:54 -0300

    Update content about dj_database_url

--- eventex/settings.py
+++ eventex/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from decouple import config
-from dj_database_url import parse as dburl
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
